26f511a177d64448e7d56374e16ed36eb6ea435c
SuslaMaksim <susla.maksim@gmail.com>
2020-06-12 13:41:21 +0300

    Updates

---
+++ precache-manifest.933df079a3af9a8a29d873dd35b114bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecd4d349ff40957a535d62650788d81",
+    "url": "/pokemon-desk/index.html"
+  },
+  {
+    "revision": "10a6e90cd11923f0c95a",
+    "url": "/pokemon-desk/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "54ed0e36df64b896d57d",
+    "url": "/pokemon-desk/static/js/2.7f80c2bb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokemon-desk/static/js/2.7f80c2bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a6e90cd11923f0c95a",
+    "url": "/pokemon-desk/static/js/main.2b0f641f.chunk.js"
+  },
+  {
+    "revision": "2cf334ade33434b674b0",
+    "url": "/pokemon-desk/static/js/runtime-main.6521782b.js"
+  }
+]);
